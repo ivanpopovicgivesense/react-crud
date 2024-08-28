a3fe5bbb4961b0ce9683a280639a6be4c2a9d688
ivanpopovicgivesense <Ivan.Popovic@givesense.rs>
2024-08-28 12:28:18 +0200

    fixed form css

--- react-crud/src/pages/CreateUserForm.tsx
+++ react-crud/src/pages/CreateUserForm.tsx
@@ -19,7 +19,6 @@ export const CreateUserForm: React.FC = () => {
   });
 
   const API_URL = "http://localhost:3000/person/";
-
   const navigate = useNavigate();
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -36,153 +35,209 @@ export const CreateUserForm: React.FC = () => {
   };
 
   return (
-    <>
-      {isLoading && (
-        <Spinner
-          style={{ marginTop: "300px" }}
-          label="Creating user..."
-          size="large"
-        />
-      )}
-      {!isLoading && (
-        <form
-          onSubmit={handleSubmit}
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Label htmlFor="name">Name:</Label>
-          <Input
-            type="text"
-            name="Name"
-            value={formData.Name}
-            onBlur={() => setTouched({ ...touched, Name: true })}
-            onChange={(e) => {
-              setFormData({ ...formData, [e.target.name]: e.target.value });
-              setErrors(
-                validate({ ...formData, [e.target.name]: e.target.value })
-              );
-            }}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        backgroundColor: "#f5f5f5",
+        padding: "20px",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          maxWidth: "500px",
+          backgroundColor: "white",
+          padding: "20px",
+          borderRadius: "8px",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        {isLoading && (
+          <Spinner
+            style={{ display: "block", margin: "auto" }}
+            label="Creating user..."
+            size="large"
           />
-          {errors.Name && touched.Name && (
-            <p style={{ color: "red" }}>{errors.Name}</p>
-          )}
-          <br />
-          <br />
-          <Label htmlFor="surname">Surname:</Label>
-          <Input
-            type="text"
-            name="Surname"
-            value={formData.Surname}
-            onBlur={() => setTouched({ ...touched, Surname: true })}
-            onChange={(e) => {
-              setFormData({ ...formData, [e.target.name]: e.target.value });
-              setErrors(
-                validate({ ...formData, [e.target.name]: e.target.value })
-              );
+        )}
+        {!isLoading && (
+          <form
+            onSubmit={handleSubmit}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "stretch",
+              gap: "20px",
             }}
-          />
-          {errors.Surname && touched.Surname && (
-            <p style={{ color: "red" }}>{errors.Surname}</p>
-          )}
-          <br />
-          <br />
-          <Label htmlFor="userType">User Type:</Label>
-          <Input
-            type="text"
-            name="UserType"
-            value={formData.UserType}
-            onBlur={() => setTouched({ ...touched, UserType: true })}
-            onChange={(e) => {
-              setFormData({ ...formData, [e.target.name]: e.target.value });
-              setErrors(
-                validate({ ...formData, [e.target.name]: e.target.value })
-              );
-            }}
-          />
-          {errors.UserType && touched.UserType && (
-            <p style={{ color: "red" }}>{errors.UserType}</p>
-          )}
-          <br />
-          <br />
-          <Label htmlFor="date">Created Date:</Label>
-          <Input
-            type="date"
-            name="CreatedDate"
-            value={formData.CreatedDate}
-            onBlur={() => setTouched({ ...touched, CreatedDate: true })}
-            onChange={(e) => {
-              setFormData({ ...formData, [e.target.name]: e.target.value });
-              setErrors(
-                validate({ ...formData, [e.target.name]: e.target.value })
-              );
-            }}
-          />
-          {errors.CreatedDate && touched.CreatedDate && (
-            <p style={{ color: "red" }}>{errors.CreatedDate}</p>
-          )}
-          <br />
-          <br />
-          <Label htmlFor="city">City:</Label>
-          <Input
-            type="text"
-            name="City"
-            value={formData.City}
-            onBlur={() => setTouched({ ...touched, City: true })}
-            onChange={(e) => {
-              setFormData({ ...formData, [e.target.name]: e.target.value });
-              setErrors(
-                validate({ ...formData, [e.target.name]: e.target.value })
-              );
-            }}
-          />
-          {errors.City && touched.City && (
-            <p style={{ color: "red" }}>{errors.City}</p>
-          )}
-          <br />
-          <br />
-          <Label htmlFor="address">Address:</Label>
-          <Input
-            type="text"
-            name="Address"
-            value={formData.Address}
-            onBlur={() => setTouched({ ...touched, Address: true })}
-            onChange={(e) => {
-              setFormData({ ...formData, [e.target.name]: e.target.value });
-              setErrors(
-                validate({ ...formData, [e.target.name]: e.target.value })
-              );
-            }}
-          />
-          {errors.Address && touched.Address && (
-            <p style={{ color: "red" }}>{errors.Address}</p>
-          )}
-          <br />
-          <br />
-          <div style={{ display: "flex", gap: "20px" }}>
-            <Button
-              type="submit"
-              appearance="primary"
-              disabled={
-                !formData.Name ||
-                !formData.Surname ||
-                !formData.UserType ||
-                !formData.CreatedDate ||
-                !formData.City ||
-                !formData.Address
-              }
+          >
+            <div style={{ display: "flex", gap: "20px" }}>
+              <div style={{ flex: 1 }}>
+                <Label htmlFor="name">Name:</Label>
+                <Input
+                  type="text"
+                  name="Name"
+                  value={formData.Name}
+                  onBlur={() => setTouched({ ...touched, Name: true })}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    });
+                    setErrors(
+                      validate({ ...formData, [e.target.name]: e.target.value })
+                    );
+                  }}
+                  style={{ width: "100%", marginBottom: "10px" }}
+                />
+                {errors.Name && touched.Name && (
+                  <p style={{ color: "red" }}>{errors.Name}</p>
+                )}
+              </div>
+              <div style={{ flex: 1 }}>
+                <Label htmlFor="surname">Surname:</Label>
+                <Input
+                  type="text"
+                  name="Surname"
+                  value={formData.Surname}
+                  onBlur={() => setTouched({ ...touched, Surname: true })}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    });
+                    setErrors(
+                      validate({ ...formData, [e.target.name]: e.target.value })
+                    );
+                  }}
+                  style={{ width: "100%", marginBottom: "10px" }}
+                />
+                {errors.Surname && touched.Surname && (
+                  <p style={{ color: "red" }}>{errors.Surname}</p>
+                )}
+              </div>
+            </div>
+
+            <div style={{ display: "flex", gap: "20px" }}>
+              <div style={{ flex: 1 }}>
+                <Label htmlFor="userType">User Type:</Label>
+                <Input
+                  type="text"
+                  name="UserType"
+                  value={formData.UserType}
+                  onBlur={() => setTouched({ ...touched, UserType: true })}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    });
+                    setErrors(
+                      validate({ ...formData, [e.target.name]: e.target.value })
+                    );
+                  }}
+                  style={{ width: "100%", marginBottom: "10px" }}
+                />
+                {errors.UserType && touched.UserType && (
+                  <p style={{ color: "red" }}>{errors.UserType}</p>
+                )}
+              </div>
+              <div style={{ flex: 1 }}>
+                <Label htmlFor="date">Created Date:</Label>
+                <Input
+                  type="date"
+                  name="CreatedDate"
+                  value={formData.CreatedDate}
+                  onBlur={() => setTouched({ ...touched, CreatedDate: true })}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    });
+                    setErrors(
+                      validate({ ...formData, [e.target.name]: e.target.value })
+                    );
+                  }}
+                  style={{ width: "100%", marginBottom: "10px" }}
+                />
+                {errors.CreatedDate && touched.CreatedDate && (
+                  <p style={{ color: "red" }}>{errors.CreatedDate}</p>
+                )}
+              </div>
+            </div>
+
+            <div style={{ display: "flex", gap: "20px" }}>
+              <div style={{ flex: 1 }}>
+                <Label htmlFor="city">City:</Label>
+                <Input
+                  type="text"
+                  name="City"
+                  value={formData.City}
+                  onBlur={() => setTouched({ ...touched, City: true })}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    });
+                    setErrors(
+                      validate({ ...formData, [e.target.name]: e.target.value })
+                    );
+                  }}
+                  style={{ width: "100%", marginBottom: "10px" }}
+                />
+                {errors.City && touched.City && (
+                  <p style={{ color: "red" }}>{errors.City}</p>
+                )}
+              </div>
+              <div style={{ flex: 1 }}>
+                <Label htmlFor="address">Address:</Label>
+                <Input
+                  type="text"
+                  name="Address"
+                  value={formData.Address}
+                  onBlur={() => setTouched({ ...touched, Address: true })}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    });
+                    setErrors(
+                      validate({ ...formData, [e.target.name]: e.target.value })
+                    );
+                  }}
+                  style={{ width: "100%", marginBottom: "10px" }}
+                />
+                {errors.Address && touched.Address && (
+                  <p style={{ color: "red" }}>{errors.Address}</p>
+                )}
+              </div>
+            </div>
+
+            <div
+              style={{ display: "flex", gap: "10px", justifyContent: "center" }}
             >
-              Submit
-            </Button>
-            <Button onClick={() => navigate("/")} appearance="secondary">
-              Cancel
-            </Button>
-          </div>
-        </form>
-      )}
-    </>
+              <Button
+                type="submit"
+                appearance="primary"
+                disabled={
+                  !formData.Name ||
+                  !formData.Surname ||
+                  !formData.UserType ||
+                  !formData.CreatedDate ||
+                  !formData.City ||
+                  !formData.Address
+                }
+              >
+                Submit
+              </Button>
+              <Button onClick={() => navigate("/")} appearance="secondary">
+                Cancel
+              </Button>
+            </div>
+          </form>
+        )}
+      </div>
+    </div>
   );
 };
--- react-crud/src/pages/UpdateUserForm.tsx
+++ react-crud/src/pages/UpdateUserForm.tsx
@@ -19,143 +19,203 @@ export const UpdateUserForm: React.FC = () => {
   } = useUpdateForm(userId);
 
   return (
-    <>
-      {isLoading && (
-        <Spinner
-          style={{ marginTop: "300px" }}
-          label="Loading user data..."
-          size="large"
-        />
-      )}
-      {!isLoading && formData && (
-        <form
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-          onSubmit={handleSubmit}
-        >
-          <Label htmlFor="ime">Name:</Label>
-          <Input
-            type="text"
-            name="Name"
-            value={formData.Name || ""}
-            onBlur={() => setTouched({ ...touched, Name: true })}
-            onChange={(e) => {
-              setFormData({ ...formData, [e.target.name]: e.target.value });
-              setErrors(
-                validate({ ...formData, [e.target.name]: e.target.value })
-              );
-            }}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        backgroundColor: "#f5f5f5",
+        padding: "20px",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          maxWidth: "500px",
+          backgroundColor: "white",
+          padding: "20px",
+          borderRadius: "8px",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        {isLoading && (
+          <Spinner
+            style={{ display: "block", margin: "auto" }}
+            label="Loading user data..."
+            size="large"
           />
-          {errors.Name && touched.Name ? (
-            <p style={{ color: "red" }}>{errors.Name}</p>
-          ) : null}
-          <br />
-          <br />
-          <Label htmlFor="prezime">Surname:</Label>
-          <Input
-            type="text"
-            name="Surname"
-            value={formData.Surname || ""}
-            onBlur={() => setTouched({ ...touched, Surname: true })}
-            onChange={(e) => {
-              setFormData({ ...formData, [e.target.name]: e.target.value });
-              setErrors(
-                validate({ ...formData, [e.target.name]: e.target.value })
-              );
+        )}
+        {!isLoading && formData && (
+          <form
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "stretch",
+              gap: "20px",
             }}
-          />
-          {errors.Surname && touched.Surname ? (
-            <p style={{ color: "red" }}>{errors.Surname}</p>
-          ) : null}
-          <br />
-          <br />
-          <Label htmlFor="tipKorisnika">User Type:</Label>
-          <Input
-            type="text"
-            name="UserType"
-            value={formData.UserType || ""}
-            onBlur={() => setTouched({ ...touched, UserType: true })}
-            onChange={(e) => {
-              setFormData({ ...formData, [e.target.name]: e.target.value });
-              setErrors(
-                validate({ ...formData, [e.target.name]: e.target.value })
-              );
-            }}
-          />
-          {errors.UserType && touched.UserType ? (
-            <p style={{ color: "red" }}>{errors.UserType}</p>
-          ) : null}
-          <br />
-          <br />
-          <Label htmlFor="datumRodjenja">Created Date:</Label>
-          <Input
-            type="date"
-            name="CreatedDate"
-            value={formData.CreatedDate || ""}
-            onBlur={() => setTouched({ ...touched, CreatedDate: true })}
-            onChange={(e) => {
-              setFormData({ ...formData, [e.target.name]: e.target.value });
-              setErrors(
-                validate({ ...formData, [e.target.name]: e.target.value })
-              );
-            }}
-          />
-          {errors.CreatedDate && touched.CreatedDate ? (
-            <p style={{ color: "red" }}>{errors.CreatedDate}</p>
-          ) : null}
-          <br />
-          <br />
-          <Label htmlFor="grad">City:</Label>
-          <Input
-            type="text"
-            name="City"
-            value={formData.City || ""}
-            onBlur={() => setTouched({ ...touched, City: true })}
-            onChange={(e) => {
-              setFormData({ ...formData, [e.target.name]: e.target.value });
-              setErrors(
-                validate({ ...formData, [e.target.name]: e.target.value })
-              );
-            }}
-          />
-          {errors.City && touched.City ? (
-            <p style={{ color: "red" }}>{errors.City}</p>
-          ) : null}
-          <br />
-          <br />
-          <Label htmlFor="adresa">Address:</Label>
-          <Input
-            type="text"
-            name="Address"
-            value={formData.Address || ""}
-            onBlur={() => setTouched({ ...touched, Address: true })}
-            onChange={(e) => {
-              setFormData({ ...formData, [e.target.name]: e.target.value });
-              setErrors(
-                validate({ ...formData, [e.target.name]: e.target.value })
-              );
-            }}
-          />
-          {errors.Address && touched.Address && !isFormChanged ? (
-            <p style={{ color: "red" }}>{errors.Address}</p>
-          ) : null}
-          <br />
-          <br />
-          <div style={{ display: "flex", gap: "20px" }}>
-            <Button type="submit" appearance="primary" disabled={isFormChanged}>
-              Update
-            </Button>
-            <Button onClick={() => navigate("/")} appearance="secondary">
-              Cancel
-            </Button>
-          </div>
-        </form>
-      )}
-      {error && <h2 style={{ color: "red" }}>{error.message}</h2>}
-    </>
+            onSubmit={handleSubmit}
+          >
+            <div style={{ display: "flex", gap: "20px" }}>
+              <div style={{ flex: 1 }}>
+                <Label htmlFor="name">Name:</Label>
+                <Input
+                  type="text"
+                  name="Name"
+                  value={formData.Name || ""}
+                  onBlur={() => setTouched({ ...touched, Name: true })}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    });
+                    setErrors(
+                      validate({ ...formData, [e.target.name]: e.target.value })
+                    );
+                  }}
+                  style={{ width: "100%", marginBottom: "10px" }}
+                />
+                {errors.Name && touched.Name && (
+                  <p style={{ color: "red" }}>{errors.Name}</p>
+                )}
+              </div>
+              <div style={{ flex: 1 }}>
+                <Label htmlFor="surname">Surname:</Label>
+                <Input
+                  type="text"
+                  name="Surname"
+                  value={formData.Surname || ""}
+                  onBlur={() => setTouched({ ...touched, Surname: true })}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    });
+                    setErrors(
+                      validate({ ...formData, [e.target.name]: e.target.value })
+                    );
+                  }}
+                  style={{ width: "100%", marginBottom: "10px" }}
+                />
+                {errors.Surname && touched.Surname && (
+                  <p style={{ color: "red" }}>{errors.Surname}</p>
+                )}
+              </div>
+            </div>
+
+            <div style={{ display: "flex", gap: "20px" }}>
+              <div style={{ flex: 1 }}>
+                <Label htmlFor="userType">User Type:</Label>
+                <Input
+                  type="text"
+                  name="UserType"
+                  value={formData.UserType || ""}
+                  onBlur={() => setTouched({ ...touched, UserType: true })}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    });
+                    setErrors(
+                      validate({ ...formData, [e.target.name]: e.target.value })
+                    );
+                  }}
+                  style={{ width: "100%", marginBottom: "10px" }}
+                />
+                {errors.UserType && touched.UserType && (
+                  <p style={{ color: "red" }}>{errors.UserType}</p>
+                )}
+              </div>
+              <div style={{ flex: 1 }}>
+                <Label htmlFor="date">Created Date:</Label>
+                <Input
+                  type="date"
+                  name="CreatedDate"
+                  value={formData.CreatedDate || ""}
+                  onBlur={() => setTouched({ ...touched, CreatedDate: true })}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    });
+                    setErrors(
+                      validate({ ...formData, [e.target.name]: e.target.value })
+                    );
+                  }}
+                  style={{ width: "100%", marginBottom: "10px" }}
+                />
+                {errors.CreatedDate && touched.CreatedDate && (
+                  <p style={{ color: "red" }}>{errors.CreatedDate}</p>
+                )}
+              </div>
+            </div>
+
+            <div style={{ display: "flex", gap: "20px" }}>
+              <div style={{ flex: 1 }}>
+                <Label htmlFor="city">City:</Label>
+                <Input
+                  type="text"
+                  name="City"
+                  value={formData.City || ""}
+                  onBlur={() => setTouched({ ...touched, City: true })}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    });
+                    setErrors(
+                      validate({ ...formData, [e.target.name]: e.target.value })
+                    );
+                  }}
+                  style={{ width: "100%", marginBottom: "10px" }}
+                />
+                {errors.City && touched.City && (
+                  <p style={{ color: "red" }}>{errors.City}</p>
+                )}
+              </div>
+              <div style={{ flex: 1 }}>
+                <Label htmlFor="address">Address:</Label>
+                <Input
+                  type="text"
+                  name="Address"
+                  value={formData.Address || ""}
+                  onBlur={() => setTouched({ ...touched, Address: true })}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      [e.target.name]: e.target.value,
+                    });
+                    setErrors(
+                      validate({ ...formData, [e.target.name]: e.target.value })
+                    );
+                  }}
+                  style={{ width: "100%", marginBottom: "10px" }}
+                />
+                {errors.Address && touched.Address && (
+                  <p style={{ color: "red" }}>{errors.Address}</p>
+                )}
+              </div>
+            </div>
+
+            <div
+              style={{ display: "flex", gap: "10px", justifyContent: "center" }}
+            >
+              <Button
+                type="submit"
+                appearance="primary"
+                disabled={isFormChanged}
+              >
+                Update
+              </Button>
+              <Button onClick={() => navigate("/")} appearance="secondary">
+                Cancel
+              </Button>
+            </div>
+          </form>
+        )}
+        {error && <h2 style={{ color: "red" }}>{error.message}</h2>}
+      </div>
+    </div>
   );
 };
