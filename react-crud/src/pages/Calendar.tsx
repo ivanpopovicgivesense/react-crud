9f43e3d403acfea1f88a81531f5ead5171aa19e6
ivanpopovicgivesense <Ivan.Popovic@givesense.rs>
2024-09-05 13:42:30 +0200

    minor calendar fix

--- react-crud/src/pages/Calendar.tsx
+++ react-crud/src/pages/Calendar.tsx
@@ -37,7 +37,7 @@ const CalendarComponent: React.FC = () => {
           border: "1px solid #dcdcdc",
         }}
       >
-        <Calendar></Calendar>
+        <Calendar showGoToToday={false}></Calendar>
       </div>
     </div>
   );
